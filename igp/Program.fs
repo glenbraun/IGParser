1a97ec574db33e9675e4929129a8a2853aff663a
Glen Braun <glen@glenbraun.com>
2021-01-17 14:31:21 -0800

    Initial implementation in Lex/Yacc of Institutional Grammar Syntax.

---
+++ igp/Program.fs
@@ -0,0 +1,9 @@
+﻿// Build lexer and parser using: dotnet build .\IGParser.sln
+
+open System
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
